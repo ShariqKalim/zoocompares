a2b752ae4b81432b725197e2138b793c7d2fd5ac
Your Name <shariqkalim123@gmail.com>
2025-11-23 17:02:11 +0530

    added sitemap

---
+++ app/sitemap.tsx
@@ -0,0 +1,41 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://digicompares.com/",
+      lastModified: new Date(),
+    },
+     {
+      url: "https://digicompares.com/web-hosting",
+      lastModified: new Date(),
+    },
+     {
+      url: "https://digicompares.com/vpn",
+      lastModified: new Date(),
+    },
+     {
+      url: "https://digicompares.com/crm-tools",
+      lastModified: new Date(),
+    },
+     {
+      url: "https://digicompares.com/seo-tools",
+      lastModified: new Date(),
+    },
+     {
+      url: "https://digicompares.com/cloud-workspaces",
+      lastModified: new Date(),
+    },
+     {
+      url: "https://digicompares.com/e-commerce",
+      lastModified: new Date(),
+    },
+        {
+      url: "https://www.digicompares.com/grammarly-ai-assistant",
+      lastModified: new Date(),
+    },
+        {
+      url: "https://digicompares.com/ai-tools",
+      lastModified: new Date(),
+    },
+    
+  ];
+}
