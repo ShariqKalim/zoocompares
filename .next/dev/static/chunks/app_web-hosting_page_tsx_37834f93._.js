184b5fd9bdd598cfee2c9e73a8980d45d2a020ec
Your Name <shariqkalim123@gmail.com>
2025-11-24 22:17:22 +0530

    added onnpage seo to web hosting page

--- .next/dev/static/chunks/app_web-hosting_page_tsx_37834f93._.js
+++ .next/dev/static/chunks/app_web-hosting_page_tsx_37834f93._.js
@@ -2,7 +2,7 @@
     script: typeof document === "object" ? document.currentScript : undefined,
     chunks: [
   "static/chunks/app_web-hosting_page_tsx_381be1de._.js",
-  "static/chunks/node_modules_next_dist_compiled_react_583fbf25._.js"
+  "static/chunks/node_modules_next_dist_0a3ad811._.js"
 ],
     source: "dynamic"
 });
